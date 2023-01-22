2857de356c20fea0ff8426059114cea14163e669
wnsgur4092 <wnsgur4092@gmail.com>
2023-01-22 13:42:51 +1100

    git test

---
+++ Week1/Week1/Week1App.swift
@@ -0,0 +1,17 @@
+//
+//  Week1App.swift
+//  Week1
+//
+//  Created by JunHyuk Lim on 22/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct Week1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
