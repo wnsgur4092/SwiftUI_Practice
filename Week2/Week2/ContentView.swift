ba90f76ffafbe595f5612f201f5f01d505428b9a
wnsgur4092 <wnsgur4092@gmail.com>
2023-02-04 20:59:14 +1100

    troubleshooting

---
+++ Week2/Week2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Week2
+//
+//  Created by JunHyuk Lim on 4/2/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Week2/Week2/Week2App.swift
@@ -0,0 +1,17 @@
+//
+//  Week2App.swift
+//  Week2
+//
+//  Created by JunHyuk Lim on 4/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct Week2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
